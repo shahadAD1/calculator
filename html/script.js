9e6a5b7842814750754c30191edfdc6a8ba59129
shahadAD1 <73888466+shahadAD1@users.noreply.github.com>
2022-06-22 01:01:22 +0300

    add vaildition before showing the result to avoid "undifined" output

--- html/script.js
+++ html/script.js
@@ -6,9 +6,9 @@ function inputValidation(){
 }
 // document..querySelector("#AC").addEventListener('change', changeClearButtonNmae()); 
 function changeClearButtonNmae(){
-if(document.querySelector("#display").value == " "){
-    document.querySelector("#AC").value='C';
- } 
+// if(document.querySelector("#display").value == " "||document.querySelector("#display").value.length==0){
+    document.getElementById("#AC").innerHTML='C';
+//  } 
 //  else {
 //     document.querySelector("#AC").value='C';
 // }
@@ -35,21 +35,34 @@ function writeOpreator(opreater){
     document.querySelector("#display").value= document.querySelector("#display").value +opreater;
 }
 // opreater functions 
-// toggleNegativePositive to switch + and - 
-function toggleNegativePositive(){///////// may be i need to check and only add - sign without multyplaying 
-        document.querySelector("#display").value= -1*document.querySelector("#display").value ; // multiply bu -1  
+
+function deleteLastChar(){
+    var userInputLength = document.querySelector("#display").value.length;
+    var userInputString = document.querySelector("#display").value;
+    document.querySelector("#display").value=userInputString.substring(0, userInputLength - 1);
+    // if(!document.querySelector("#display").value == " "){// if empty just write the number 
+    //     document.querySelector("#display").value= document.querySelector("#display").value + 1;  
+    // }else {// if not empty add the number after the exist nuber  
+    //     document.querySelector("#display").value= document.querySelector("#display").value + ;  
+    // }
 }
 
 function showResult(){
+    if(document.querySelector("#display").value == " "||document.querySelector("#display").value.length==0){
+        document.calculator.dispaly.value=" ";
+        alert("enter input "); 
+    }else {
+        document.calculator.dispaly.value= eval(calculator.dispaly.value);  
+    }
 //    if(isNaN(eval(calculator.dispaly.value))){
 //     document.calculator.dispaly.value= "not a number"
 //    }else {
 //     document.calculator.dispaly.value= "number"  
 //    }
-var currentValue = document.calculator.dispaly.value;
-// if (eval(calculator.dispaly.value)==currentValue)
-// alert("check again")
-    document.calculator.dispaly.value= eval(calculator.dispaly.value)
+// var currentValue = document.calculator.dispaly.value;
+// // if (eval(calculator.dispaly.value)==currentValue)
+// // alert("check again")
+//     document.calculator.dispaly.value= eval(calculator.dispaly.value);
 
 }
 //to restric user input (from keyboard) to only number 
